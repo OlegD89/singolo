151adafca8318cd8a947bb4bb0b09e04b8ee6eb9
dubkooleg@gmail.com <dubkooleg@gmail.com>
2020-03-15 23:11:52 +0300

    feat: evets Portfolio

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 window.onload = function() {
     console.log('run script')
 
-    this.document.querySelector('.navigation').addEventListener('click', clickNavigation)
-    this.document.querySelector('.visual-content').addEventListener('click', clickPhoneNavigation)
-    this.document.querySelector('.visual-content').addEventListener('click', clickPhone)
+    document.querySelector('.navigation').addEventListener('click', clickNavigation)
+    document.querySelector('.visual-content').addEventListener('click', clickPhoneNavigation)
+    document.querySelector('.visual-content').addEventListener('click', clickPhone)
+    document.querySelector('.gallery-filters').addEventListener('click', clickGalleryFilters)
+    document.querySelector('.gallery-images').addEventListener('click', clickGalleryImage)
 }
-
+//#region Header
 const clickNavigation = (e) => {
-    if(e.target.classList.contains('navigation-link__text')){
-        if(e.target.classList.contains('navigation-link_active')){
-            return;
-        }
-        removeActive(e.currentTarget, 'navigation-link_active');
-        setActive(e.target, 'navigation-link_active');
-    }
+    changeActive(e, 'navigation-link__text', 'navigation-link_active');
 }
+//#endregion Header
 
+//#region VisualContent
 const clickPhoneNavigation = (e) => {
     if(e.target.classList.contains('navigation__icon')){
         let isMoveLeft = e.target.classList.contains('navigation__icon_left');
@@ -27,14 +25,6 @@ const clickPhone = (e) => {
     if(e.target.classList.contains('phone__base')){
         changePhonePower(e.target);
     }
-}
-
-const removeActive = (container, classActiveName) => {
-    let el = container.querySelector(`.${classActiveName}`);
-    el.classList.remove(classActiveName);
-}
-const setActive = (target, classActiveName) => {
-    target.classList.add(classActiveName);
 }
 
 const changePhoneScreen = (isLeft) => {
@@ -59,3 +49,51 @@ const changePhonePower = (phoneBase) => {
         phoneScreen.classList.add('phone__screen-off');
     }
 }
+//#endregion VisualContent
+
+//#region Portfolio
+const clickGalleryFilters = (e) => {
+    changeActive(e, 'gallery__tag', 'tag_ative', 
+        () => { changeGalary(e.currentTarget, e.target.innerText); });
+}
+
+const changeGalary = (blockFilters, setFilter) => {
+    let galleryImages = blockFilters.closest('.gallery').querySelector('.gallery-images');
+    let images = galleryImages.children;
+    //TODO Сделать сортировку с помощью массивов
+    // let array = [].slice.call(images);
+    // let el = array[1];
+    // array.splice(el, 1);
+    // array.push(el);
+
+    galleryImages.append(images[0]);
+}
+
+const clickGalleryImage = (e) => {  
+    changeActive(e, 'gallery-image', 'gallery-image_active');
+}
+//#endregion Portfolio
+
+
+//#region Base
+const changeActive = (e, classElement, classActiveElement, success) => {
+    let target = e.target;
+    if(target.classList.contains(classElement)){
+        if(target.classList.contains(classActiveElement)){
+            return;
+        }
+        removeActive(e.currentTarget, classActiveElement);
+        setActive(target, classActiveElement);
+        success && success();
+    }
+}
+
+const removeActive = (container, classActiveName) => {
+    let el = container.querySelector(`.${classActiveName}`);
+    el &&
+    el.classList.remove(classActiveName);
+}
+const setActive = (target, classActiveName) => {
+    target.classList.add(classActiveName);
+}
+//#endregion Base
