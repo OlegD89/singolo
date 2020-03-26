1345e6c19bdc66631d11d1961a8f26f962d61bea
dubkooleg@gmail.com <dubkooleg@gmail.com>
2020-03-26 14:02:13 +0300

    feat: responsive portfolio, new resorting images, reset form

--- script.js
+++ script.js
@@ -166,14 +166,15 @@ const clickGalleryFilters = (e) => {
 
 const changeGalary = (blockFilters, setFilter) => {
     let galleryImages = blockFilters.closest('.gallery').querySelector('.gallery-images');
-    let images = galleryImages.children;
-    //TODO Сделать сортировку с помощью массивов
-    // let array = [].slice.call(images);
-    // let el = array[1];
-    // array.splice(el, 1);
-    // array.push(el);
 
-    galleryImages.append(images[0]);
+    let images = Array.from(galleryImages.children);
+    images = images.sort(function(){
+        return Math.random() - 0.5;
+      });
+    images.forEach(image=>galleryImages.append(image));
+
+    // простое выполнение задания(переставляем первый элемент в конец):
+    // galleryImages.append(galleryImages.children[0]);
 }
 
 const clickGalleryImage = (e) => {  
@@ -193,6 +194,7 @@ const clickQuoteButton = (e) => {
     setQuoteTheme();
     setQuoteDesc();
     showQuoteModal();
+    clearQueryForm();
 
     e.preventDefault();
 }
@@ -208,13 +210,15 @@ const setQuoteTheme = (e) => {
     let modalTheme = document.querySelector('.modal-quote__theme');
     let theme = document.querySelector('.quote__subject');
     modalTheme.innerHTML = theme.value?`Тема: ${theme.value}`:'Без темы';
-    theme.value = '';
 }
 const setQuoteDesc = (e) => {
     let modalDesc = document.querySelector('.modal-quote__desc');
     let desc = document.querySelector('.quote__describe');
     modalDesc.innerHTML = desc.value?`Описание: ${desc.value}`:'Без описания';
-    desc.value = '';
+}
+const clearQueryForm = () => {
+    let form = document.querySelector('.quote__form');
+    form.reset();
 }
 const showQuoteModal = (e) => {
     let modal = document.querySelector('.overlay_modal-quote');
